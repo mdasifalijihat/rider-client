b9972bd6768010b71192d502fa654580d7b8f65f
mdasifalijihat <mdasifalijihat@gmail.com>
2025-07-04 23:58:13 +0600

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../pages/home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children: [
+        { index: true, Component: Home }, 
+
+
+    ],
+  },
+]);
